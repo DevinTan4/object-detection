44367f9bcf7f254d066904387619e7ae09eaf8ed
DasonTiovino <codingdec14@gmail.com>
2024-04-23 13:13:18 +0700

    feat: create api

--- yolo-car-counter.py
+++ yolo-car-counter.py
@@ -6,7 +6,30 @@ import time
 import serial
 from sort import *
 import numpy as np
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from threading import Thread, Timer
+from multiprocessing import Process, Manager
+import time
+
+app = Flask(__name__)
+CORS(app)
+motorbike_count = 0
+
+@app.route('/api/motorbike', methods=['GET'])
+def get_motorbikeCount():
+    global motorbike_count
+    return jsonify({'motorbike_count': motorbike_count})
+
+def update_motorbike_count(count):
+    global motorbike_count
+    motorbike_count = count
+
+def run_flask_app():
+    app.run(debug=True, use_reloader=False, port=8080)
+
+flask_thread = Thread(target=run_flask_app)
+flask_thread.start()
 
 cap = cv2.VideoCapture('asset/videos/pradita-vehicle-counting.mp4') # For Video
 # cap.set(4, 720)
@@ -45,12 +68,6 @@ baud_rate = 9600
 ser = serial.Serial(serial_port, baud_rate)
 time.sleep(2)
 
-# def control_led(color):
-#     if color == 'yellow':
-#         ser.write(b'Y')
-#     elif color == 'red':
-#         ser.write(b'R')
-
 def control_led(color):
     if color == 'green':
         ser.write(b'G')
@@ -67,8 +84,8 @@ while True:
 
     carGraphics = cv2.imread("asset/images/car-graphics.png", cv2.IMREAD_UNCHANGED)
     motorbikeGraphics = cv2.imread("asset/images/motorbike-graphics.png", cv2.IMREAD_UNCHANGED)
-    img = cvzone.overlayPNG(img,carGraphics, (0,0))
-    img = cvzone.overlayPNG(img,motorbikeGraphics, (0,162))
+    img = cvzone.overlayPNG(img, carGraphics, (0, 0))
+    img = cvzone.overlayPNG(img, motorbikeGraphics, (0, 162))
 
     results = model(imgRegion, stream=True)
 
@@ -76,40 +93,30 @@ while True:
     for r in results:
         boxes = r.boxes
         for box in boxes:
-            # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-            # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
             cls = int(box.cls[0])
 
             currentClass = classNames[cls]
             if currentClass == "car" or currentClass == "bus" or currentClass == "truck" or currentClass == "motorbike" and conf > 0.3:
-                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
                 cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
-    # fps = 1 / (new_frame_time - prev_frame_time)
-    # prev_frame_time = new_frame_time
-    # print(fps)
-
     resultsTracker = tracker.update(detections)
-    cv2.line(img, (limitsIn[0],limitsIn[1]), (limitsIn[2],limitsIn[3]), (0,0,255), thickness=5)
+    cv2.line(img, (limitsIn[0], limitsIn[1]), (limitsIn[2], limitsIn[3]), (0, 0, 255), thickness=5)
 
     for result in resultsTracker:
-        x1,y1,x2,y2,id = result
+        x1, y1, x2, y2, id = result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        print(result)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,0))
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
         cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)), scale=2, thickness=3, offset=10)
 
-        cx,cy = x1+w//2,y1+h//2
-        cv2.circle(img, (cx,cy), 5, (255,0,255), cv2.FILLED)
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
         if limitsIn[0] < cx < limitsIn[2] and limitsIn[1] - 20 < cy < limitsIn[1] + 20:
             if cls == 2 or cls == 7:
@@ -119,12 +126,6 @@ while True:
                 if id not in motorbikeCount:
                     motorbikeCount.append(id)
             cv2.line(img, (limitsIn[0], limitsIn[1]), (limitsIn[2], limitsIn[3]), (0, 255, 0), thickness=5)
-
-    # Control LED based on carCount
-    # if len(motorbikeCount) == 1:
-    #     control_led('red')
-    # elif len(motorbikeCount) < 1:
-    #     control_led('yellow')
 
     # Control LED based on motorbikeCount and carCount
     if len(motorbikeCount) < 1 and len(carCount) < 1:
@@ -141,3 +142,6 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+    update_motorbike_count(len(motorbikeCount))
+
